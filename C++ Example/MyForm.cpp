6cbe09926e066427eda4cdda5fe0965a9fe51e36
Copley Controls <117777616+CopleyControlsOfficial@users.noreply.github.com>
2022-11-09 14:24:31 -0500

    Create MyForm.cpp

---
+++ C++ Example/MyForm.cpp
@@ -0,0 +1,14 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	serialexample::MyForm form;
+	Application::Run(% form);
+}
+
+
